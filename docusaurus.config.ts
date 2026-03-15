5cbba09137ae0ba3d561909c691f3cea27ceab2b
Qasim04026 <w.ifti@yahoo.com>
2026-03-16 03:06:31 +0500

    Fix url string

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,12 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://qasim04026.github.io` ,
+  
+  url: 'https://qasim04026.github.io',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/,physical-ai-humanoid-robotics-textbook/',
+  baseUrl:   '/physical-ai-humanoid-robotics-textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
