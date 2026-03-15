7a26d92f009792fb5f3704cd3e93e5ee0bbfa8bb
Qasim04026 <w.ifti@yahoo.com>
2026-03-15 23:20:38 +0500

    Fix sidebar IDs

--- sidebars.ts
+++ sidebars.ts
@@ -22,11 +22,11 @@ const sidebars: SidebarsConfig = {
     'chapter_5_robot_simulation_gazebo',
     'chapter_6_unity_for_robotics',
     'chapter_7_nvidi-isaac_platform',
-    'chapter_8_humanoid_robot_development',
-    'chapter_9_vision_language_action_models',
-    'chapter_10_conversational_robotics',
-    'chapter_11_sensor_systems',
-    'chapter_12_capstone_project',
+    'chapter_8_Humanoid_Robot_Development',
+    'chapter_9_Vision_Language_Action_Models',
+    'chapter_10_Conversational_Robotics',
+    'chapter_11_Sensor_Systems',
+    'chapter_12_Capstone_Project',
   ],
 
   // But you can create a sidebar manually
